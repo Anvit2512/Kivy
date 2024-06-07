4d31febd2d349d53b0ee8797fc39da374694f9ee
Anvit2512 <130550310+Anvit2512@users.noreply.github.com>
2024-06-07 21:32:05 +0530

    database.py

--- database.py
+++ database.py
@@ -1,31 +1,51 @@
+import datetime
+
+
 class DataBase:
     def __init__(self, filename):
         self.filename = filename
-        self.users = {}
+        self.users = None
+        self.file = None
         self.load()
 
     def load(self):
-        with open(self.filename, "r") as file:
-            for line in file:
-                email, password, name, created = line.strip().split(";")
-                self.users[email] = (password, name, created)
+        self.file = open(self.filename, "r")
+        self.users = {}
 
-    def add_user(self, email, password, name):
-        if email not in self.users:
-            self.users[email] = (password, name, "2024-06-04")  # Assuming creation date is today
-            self.save()
+        for line in self.file:
+            email, password, name, created = line.strip().split(";")
+            self.users[email] = (password, name, created)
 
-    def validate(self, email, password):
-        if email in self.users:
-            return self.users[email][0] == password
-        return False
+        self.file.close()
 
     def get_user(self, email):
         if email in self.users:
             return self.users[email]
-        return None
+        else:
+            return -1
+
+    def add_user(self, email, password, name):
+        if email.strip() not in self.users:
+            self.users[email.strip()] = (password.strip(), name.strip(), DataBase.get_date())
+            self.save()
+            return 1
+        else:
+            print("Email exists already")
+            return -1
+
+    def validate(self, email, password):
+        if self.get_user(email) != -1:
+            return self.users[email][0] == password
+        else:
+            return False
 
     def save(self):
-        with open(self.filename, "w") as file:
-            for email, (password, name, created) in self.users.items():
-                file.write(f"{email};{password};{name};{created}\n")
+        with open(self.filename, "w") as f:
+            for user in self.users:
+                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+
+    @staticmethod
+    def get_date():
+        return str(datetime.datetime.now()).split(" ")[0]
+
+
